7d4e02846144dbde682041af6805a7dbbfd2ef00
James Trayford <j.w.trayford@port.ac.uk>
2025-11-10 16:12:06 +0000

    update

---
+++ __init__.py
@@ -0,0 +1,62 @@
+from strauss import Synthesizer, Score, Sonification, Objects, Events, 
+import matplotlib.pyplot
+import pandas as pd
+
+duration = {"shortest": 4.,
+            "short": 8.,
+            "medium": 16.,
+            "long": 32.,
+            "longest": 64.}
+
+
+def setup():
+    
+
+def sonify_chord(time, brightness, dur):
+    pass
+
+def sonify_notes(time, brightness, dur):
+    pass
+
+def sonify_wind(time, brightness, dur):
+    generator.modify_preset({'filter':'on',
+                         'looping':'forwardback',
+                         'loop_start': 0.8, 'loop_end': 1.2}) # ✏️ for such a fast sequence, using ~10 ms values
+
+    generator = Synthesizer()
+    generator.load_preset('windy')
+
+    # we use a 'chord' here to create more harmonic richness (stacking fifths)...
+    notes = [["A2"]]
+    score =  Score(notes, dur)
+
+    data = {'pitch':[0],
+            'time_evo':[time],
+            'cutoff':[brightness]}
+
+    lims = {'time_evo': ('0','100'),
+            'cutoff': ('0','100')}
+    
+    sources = Objects(data.keys())
+    sources.fromdict(data)
+    plims = {'cutoff': (0.15,0.95)}
+    sources.apply_mapping_functions(map_lims=lims, param_lims=plims)
+
+    soni = Sonification(score, sources, generator, system)
+    soni.render()
+    dobj = soni.notebook_display(show_waveform=0)
+
+def sonify_lightcurve(time, brightness, style='wind', length='medium'):
+    if length in duration.keys():
+        dur = duration[length]
+    else:
+        raise Exception(f"length {length} not supported!")
+    if style == 'wind':
+        sonify_wind(time, brightness, dur)
+    elif style == 'chord':
+        sonify_chord(time, brightness, dur)
+    elif style == 'notes':
+        sonify_notes(time, brightness, dur)
+    else:
+        raise Exception(f"style {style} not supported!")
+    
